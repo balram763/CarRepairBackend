188efb45edcc8e82d9496f45b3b46d971e8346c0
balram <85399069+balram763@users.noreply.github.com>
2025-03-16 23:46:11 +0530

    Update userController.js

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,99 +1,3 @@
-// const expressAsyncHandler = require("express-async-handler")
-// const User = require("../model/userModel")
-// let bcrypt = require('bcryptjs');
-// let jwt = require('jsonwebtoken');
-
-
-// const registerUser = expressAsyncHandler(async(req,res) => {
-//     const {name , email , password, isAdmin} = req.body
-
-//     if(!name || !email || !password){
-//         res.status(400)
-//         throw new Error("please Fill All Details")
-//     }
-//     const userExist = await User.findOne({email : email})
-//     if(userExist){
-//         res.status(400)
-//         throw new Error("user already exist")
-//     }
-
-//     // pswd encryption
-
-//     let salt = bcrypt.genSaltSync(10);
-//     let hashPassword = bcrypt.hashSync(password, salt);
-
-//     const user = await User.create({
-//         name,
-//         email,
-//         password : hashPassword,
-//     })
-
-//     if(!user){
-//         res.status(400)
-//         throw new Error("user is not created")
-//     }
-
-//     res.status(201)
-//     res.json({
-//         id : user._id,
-//         name : user.name,
-//         email : user.email,
-//         token : generateToken(user._id),
-//         isAdmin : user.isAdmin
-//     })
-//     res.send("register successfully")
-// })
-
-
-
-// const loginUser = expressAsyncHandler(async (req, res) => {
-//     const { email, password } = req.body;
-
-//     if (!email || !password) {
-//         return res.status(400).json({ message: "Fill all details" });
-//     }
-
-//     const user = await User.findOne({ email });
-
-//     if (!user) {
-//         return res.status(400).json({ message: "USER NOT FOUND SIGN UP FIRST" });
-//     }
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-    
-//     if (!isMatch) {
-//         return res.status(400).json({ message: "Invalid credentials" });
-//     }
-
-//     res.status(200).json({
-//         id: user._id,
-//         name: user.name,
-//         email: user.email,
-//         token: generateToken(user._id),
-//         isAdmin: user.isAdmin,
-//     });
-// });
-
-
-// //Genetate token 
-
-// const generateToken = (id) =>{
-//     return jwt.sign({id},process.env.JWT_SECRET)
-// }
-
-// //private
-
-// const privateController = expressAsyncHandler(
-//     async(req,res) => {
-//         res.json({
-//             msg : "private route"
-//         })
-//     }
-// )
-
-
-// module.exports = {registerUser,loginUser,privateController}
-
 const expressAsyncHandler = require("express-async-handler");
 const User = require("../model/userModel");
 const bcrypt = require("bcryptjs");
@@ -141,12 +45,10 @@ const registerUser = expressAsyncHandler(async (req, res) => {
 const loginUser = expressAsyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
-<<<<<<< HEAD
-=======
+
 const loginUser = expressAsyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
->>>>>>> 0a9d18464ddd43be118737f46f99ffb62aca1167
     if (!email || !password) {
         return res.status(400).json({ message: "Fill all details" });
     }
@@ -154,18 +56,9 @@ const loginUser = expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-<<<<<<< HEAD
         return res.status(400).json({ message: "User not found. Sign up first" });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-        return res.status(400).json({ message: "Invalid credentials" });
-    }
-=======
-        return res.status(400).json({ message: "USER NOT FOUND SIGN UP FIRST" });
-    }
 
     const isMatch = await bcrypt.compare(password, user.password);
     
@@ -181,7 +74,6 @@ const loginUser = expressAsyncHandler(async (req, res) => {
         isAdmin: user.isAdmin,
     });
 });
->>>>>>> 0a9d18464ddd43be118737f46f99ffb62aca1167
 
     res.status(200).json({
         id: user._id,
@@ -202,9 +94,7 @@ const privateController = expressAsyncHandler(async (req, res) => {
     res.json({ message: "Private route accessed successfully" });
 });
 
-<<<<<<< HEAD
 module.exports = { registerUser, loginUser, privateController };
-=======
 const privateController = expressAsyncHandler(
     async(req,res) => {
         res.json({
@@ -215,4 +105,3 @@ const privateController = expressAsyncHandler(
 
 
 module.exports = {registerUser,loginUser,privateController}
->>>>>>> 0a9d18464ddd43be118737f46f99ffb62aca1167
